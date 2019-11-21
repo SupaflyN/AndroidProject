428c5693ca117b1ae827f1e645339606d8577904
SupaflyN <potatogroupth@gmail.com>
2019-11-21 18:38:43 +0700

    ใส่ตัวอักษรมั่วๆ

--- app/src/main/java/com/example/app/QuestionActivity.java
+++ app/src/main/java/com/example/app/QuestionActivity.java
@@ -32,7 +32,7 @@ public class QuestionActivity extends AppCompatActivity {
         tv = (TextView) findViewById(R.id.textShowScore);
         Button Submit = (Button)findViewById(R.id.buttonSubmit);
         Submit.setOnClickListener(new View.OnClickListener() {
-            //55556666666
+            //55556666666//55556666666
 //55556666666
                                       @Override
                                       public void onClick(View view) {
