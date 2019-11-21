9c76971d1998c4bf43cdbf2296c830bf1d9cea17
SupaflyN <potatogroupth@gmail.com>
2019-11-21 18:34:35 +0700

    แก้ทดสอบ

--- app/src/main/java/com/example/app/QuestionActivity.java
+++ app/src/main/java/com/example/app/QuestionActivity.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.Gravity;
@@ -30,24 +31,26 @@ public class QuestionActivity extends AppCompatActivity {
 
         tv = (TextView) findViewById(R.id.textShowScore);
         Button Submit = (Button)findViewById(R.id.buttonSubmit);
-        Submit.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view){
-                int score = 0;
-                if (((RadioButton)findViewById(R.id.radioButton1)).isChecked()) score += 1;
-                if (((RadioButton)findViewById(R.id.radioButton2)).isChecked()) score += 2;
-                if (((RadioButton)findViewById(R.id.radioButton3)).isChecked()) score += 3;
-                if (((RadioButton)findViewById(R.id.radioButton4)).isChecked()) score += 4;
-
-
-                if (((RadioButton)findViewById(R.id.radioButton11)).isChecked()) score += 1;
-                if (((RadioButton)findViewById(R.id.radioButton22)).isChecked()) score += 2;
-                if (((RadioButton)findViewById(R.id.radioButton33)).isChecked()) score += 3;
-                if (((RadioButton)findViewById(R.id.radioButton44)).isChecked()) score += 4;
+        Submit.setOnClickListener(new View.OnClickListener() {
+//5555
+                                      @Override
+                                      public void onClick(View view) {
+                                          int score = 0;
+                                          if (((RadioButton) findViewById(R.id.radioButton1)).isChecked()) score += 1;
+                                          if (((RadioButton) findViewById(R.id.radioButton2)).isChecked()) score += 2;
+                                          if (((RadioButton) findViewById(R.id.radioButton3)).isChecked()) score += 3;
+                                          if (((RadioButton) findViewById(R.id.radioButton4)).isChecked()) score += 4;
 
 
-               displayResult(score);
+                                          if (((RadioButton) findViewById(R.id.radioButton11)).isChecked()) score += 1;
+                                          if (((RadioButton) findViewById(R.id.radioButton22)).isChecked()) score += 2;
+                                          if (((RadioButton) findViewById(R.id.radioButton33)).isChecked()) score += 3;
+                                          if (((RadioButton) findViewById(R.id.radioButton44)).isChecked()) score += 4;
+
+                 if ( score >= 3 ) {
+                     Intent intent = new Intent(getApplicationContext(), ScopeActivity.class);
+                     startActivity(intent);
+                 }
             }
 
         });}
