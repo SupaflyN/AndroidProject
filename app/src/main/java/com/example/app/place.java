ce760ce748b9ddf0f17a3111420206c38f6d32bc
SupaflyN <potatogroupth@gmail.com>
2019-11-28 23:30:52 +0700

    new ui risk

--- app/src/main/java/com/example/app/place.java
+++ app/src/main/java/com/example/app/place.java
@@ -2,13 +2,24 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class place extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_place);
+        setContentView(R.layout.activity_cure);
+        ImageButton home = (ImageButton) findViewById(R.id.home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
