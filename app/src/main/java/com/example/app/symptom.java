6f74e02cec3f1a805eb74c99dfc756b13ebc0615
SupaflyN <potatogroupth@gmail.com>
2019-11-28 18:48:49 +0700

    เมนูมั่ว

--- app/src/main/java/com/example/app/symptom.java
+++ app/src/main/java/com/example/app/symptom.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class cause extends AppCompatActivity {
+public class symptom extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cause);
+        setContentView(R.layout.activity_symptom);
     }
 }
