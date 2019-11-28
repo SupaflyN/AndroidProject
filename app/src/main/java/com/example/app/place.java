bda4d924c948b932ddf8ffc25cdb9cc49eaa7e07
SupaflyN <potatogroupth@gmail.com>
2019-11-28 18:52:56 +0700

    yoyo

--- app/src/main/java/com/example/app/place.java
+++ app/src/main/java/com/example/app/place.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class type extends AppCompatActivity {
+public class place extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_type);
+        setContentView(R.layout.activity_place);
     }
 }
